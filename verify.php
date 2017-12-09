9c1dd351f7716f4e4425d032e930bd29eb4fe46f
seksan mankhong <seksan_52@hotmail.com>
2017-12-09 15:18:12 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'GJrz7Hx2DmpPBDzfKR3VCOl/2SAxAX8X1yS7tA07A+uF139Pm2TDN03HinwbEz+Z3rYzPdwQ9v08GuPdKAG0k2HbmdGGqakr5Kk1Xx6THRwm7E1cjRlSoovRqcVHqaasRyWO3mNiYIhmCSsyFsJ0sgdB04t89/1O/w1cDnyilFU=';
+$access_token = '7X0WpZEFwwM84n8hfVcTeRvhYJheIud7IFoAwpcpPUXbu6ZyiD4v9KkarQbId6Zx3rYzPdwQ9v08GuPdKAG0k2HbmdGGqakr5Kk1Xx6THRyJGndFalJjpepEd6Z136QXyM2arBV6CAUjHYKxWDmbiAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
