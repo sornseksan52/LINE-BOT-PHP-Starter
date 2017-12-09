dd534faec81b779b1f2b8faf00f42875148a490a
seksan mankhong <seksan_52@hotmail.com>
2017-12-09 15:48:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,8 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'สวัสดีครับ',
-				'text' => print_r($event,true),
-				'talk' => $text
+				'text' => print_r($event,true)
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
